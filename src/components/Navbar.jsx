adbd7ad66c859f19b2d8ecf523a27c0d1a72afe7
bernadettechrestella <bernadettechrestella@gmail.com>
2023-10-05 11:58:26 +0900

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <header>
         <nav className='flex justify-between items-center w-full px-20 h-20 z-10 absolute'>
           <div>
-            <p className='text-2xl font-semibold flex text-green-600'>CULINARY
-                <span className='text-orange-500'>PASSPORT</span>
-            </p>
+            <Link to='/'>
+              <p className='text-2xl font-semibold flex text-green-600 cursor-pointer'>CULINARY
+                  <span className='text-orange-500'>PASSPORT</span>
+              </p>
+            </Link>
           </div>
         </nav>
     </header>
