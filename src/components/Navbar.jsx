2f74b7106df86768f2759b783b7abb5fc8d27f54
bernadettechrestella <bernadettechrestella@gmail.com>
2023-10-05 14:09:26 +0900

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,17 @@ const Navbar = () => {
   return (
     <header>
         <nav className='flex justify-between items-center w-full px-20 h-20 z-10 absolute'>
-          <div>
-            <Link to='/'>
-              <p className='text-2xl font-semibold flex text-green-600 cursor-pointer'>CULINARY
-                  <span className='text-orange-500'>PASSPORT</span>
-              </p>
-            </Link>
-          </div>
+            <div>
+              <Link to='/'>
+                <p className='text-2xl font-semibold flex text-green-600 cursor-pointer'>CULINARY
+                    <span className='text-orange-500'>PASSPORT</span>
+                </p>
+              </Link>
+            </div>
+            {/* <div className='flex gap-4 text-xl font-bold'>
+              <p className='text-green-500'>Recipes</p>
+              <p className='text-orange-500'>Ingredients</p>
+            </div> */}
         </nav>
     </header>
   )
