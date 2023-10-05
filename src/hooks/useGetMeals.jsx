c8505176be35c7e1251ddd2b8af170f885f26484
bernadettechrestella <bernadettechrestella@gmail.com>
2023-10-05 11:58:30 +0900

    Update useGetMeals.jsx

--- src/hooks/useGetMeals.jsx
+++ src/hooks/useGetMeals.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
-import { getIngredients, getMealsByLetter } from "../services/meals.services"
+import { getIngredients, getMealsById, getMealsByLetter } from "../services/meals.services"
+import { useParams } from "react-router-dom"
 
 export const useGetMeals = () => {
     const [meals, setMeals] = useState([])
