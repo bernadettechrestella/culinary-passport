4f4128703ed1dd4aca8dbb088a69852b2a83ba39
bernadettechrestella <bernadettechrestella@gmail.com>
2023-12-02 12:02:53 +0900

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Link as ScrollLink} from 'react-scroll'
 import { FaHome } from "react-icons/fa";
+import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai";
 
 const Navbar = () => {
   const location = useLocation()
 
+  const [nav, setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
+  const handleClose = () => {
+    setNav(!nav)
+  }
+
   return (
     <header>
-        <nav className='flex justify-between items-center w-full px-20 tablet:h-16 h-12 z-10 shadow-md fixed bg-white'>
+        <nav className='flex justify-between items-center w-full px-20 tablet:h-16 h-12 z-20 shadow-md fixed bg-white'>
             <div>
               <Link to='/'>
                 <p className='text-2xl font-semibold flex text-green-600 cursor-pointer'>CULINARY
@@ -16,7 +27,8 @@ const Navbar = () => {
                 </p>
               </Link>
             </div>
-            <div className='flex gap-4 text-xl cursor-pointer'>
+
+            <div className='hidden tablet:flex gap-4 text-xl cursor-pointer'>
               {location.pathname !== '/' ? (
                 <>
                   <Link to='/'>
@@ -36,7 +48,37 @@ const Navbar = () => {
                 </>
               )}
             </div>
+
+            <div className='tablet:hidden cursor-pointer ml-10'>
+              {location.pathname !== '/' ? (
+                  <Link to='/'>
+                    <FaHome size={25} className='text-green-500'/>
+                  </Link>
+              ) : (
+                <>
+                  {nav ? (
+                    <AiOutlineClose size={20} onClick={handleClose} className='text-orange-500'/>
+                  ) : (
+                    <AiOutlineMenu size={20} onClick={handleNav} className='text-green-500'/>
+                  )}
+                </>
+              )}
+            </div>
         </nav>
+
+        {/* Mobile Menu */}
+        <div className={nav ? 'fixed right-0 top-0 w-[60%] h-full z-10 border-l border-l-green-500 bg-white ease-in-out duration-500' : 'fixed right-[-100%]'}>
+            <div className='flex flex-col justify-center items-center gap-4 text-xl cursor-pointer mt-16'>
+                  <ScrollLink to='recipes' smooth={true} duration={500}>
+                    <p className='text-green-500' onClick={handleClose}>Recipes</p>
+                  </ScrollLink>
+                  <ScrollLink to='ingredients' smooth={true} duration={500}>
+                    <p className='text-orange-500' onClick={handleClose}>Ingredients</p>
+                  </ScrollLink>
+                    <p className='text-green-500' onClick={handleClose}>Categories</p>
+                    <p className='text-orange-500' onClick={handleClose}>Countries</p>
+            </div>
+        </div>
     </header>
   )
 }
